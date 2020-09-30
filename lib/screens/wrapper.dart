3f0fc2a64e0c3a1bda3c96d41cca652a68d66f8e
Siddhant Mishra <37289521+sidjmishra@users.noreply.github.com>
2020-10-01 01:36:56 +0530

    Update wrapper.dart

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -7,13 +7,5 @@ import 'package:provider/provider.dart';
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<User>(context);
-    if(user == null){
-      return Authenticate();
-    }
-    else {
-      return Home();
-    }
-
   }
 }
